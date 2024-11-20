a254f633651948e07cd742dab3e8ba5394ca1e21
Vijay Shankar R <rvjshankar35@gmail.com>
2024-11-20 23:48:35 +0530

    Introduce a bug and failing test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # main.py
-def greet(name):
-    return f"Hello, {name}!"
+def greet(name, excited=False):
+    if excited:
+        return f"Hello, {name}"
+    return f"Hi, {name}!"
 
 if __name__ == "__main__":
     print(greet("World"))
--- test_main.py
+++ test_main.py
@@ -1,6 +1,9 @@
 #test_main.py
 from main import greet
 
-def test_greet():
+#def test_greet():
+#    assert greet("Alice") == "Hello, Alice!"
+#    assert greet("Bob") == "Hello, Bob!"
+
+def test_greet_fails():
     assert greet("Alice") == "Hello, Alice!"
-    assert greet("Bob") == "Hello, Bob!"
